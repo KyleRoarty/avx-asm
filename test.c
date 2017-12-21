8345387559b3eba2e595e18a508616a98cab2062
Kyle Roarty <kroarty@xes-inc.com>
2017-12-21 07:54:51 -0600

    test: Function for printing char arrays, declare asm functions as extern.
    Signed-off-by: Kyle Roarty <kroarty@xes-inc.com>

--- test.c
+++ test.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+extern void expandkey128(unsigned char *, unsigned char *);
+extern void aes_encrypt_asm(unsigned char *, unsigned char *, int);
+extern void aes_decrypt_asm(unsigned char *, unsigned char *, int);
+
+// len = sizeof(char_str). Normalized inside of function
+void printCharStr(unsigned char *char_str, int len){
+    for(int i = 0; i < len/sizeof(unsigned char); i++){
+        printf("%02X", char_str[i]);
+    }
+    printf("\n");
+
+    return;
+}
+
 int main( ){
     unsigned char key[16] = {0x00  ,0x01  ,0x02  ,0x03  ,0x04  ,0x05  ,0x06  ,0x07,
                              0x08  ,0x09  ,0x0a  ,0x0b  ,0x0c  ,0x0d  ,0x0e  ,0x0f};
@@ -7,23 +21,15 @@ int main( ){
                              0x88  ,0x99  ,0xaa  ,0xbb  ,0xcc  ,0xdd  ,0xee  ,0xff};
     unsigned char RoundKey[240];
     int Nr = 10;
-    for(int i = 0; i < sizeof(key)/sizeof(unsigned char); i++){
-        printf("%02X",key[i]);
-    }
-    printf("\n");
-    for(int i = 0; i < sizeof(ptext)/sizeof(unsigned char); i++){
-        printf("%02X",ptext[i]);
-    }
-    printf("\n");
+
+    printCharStr(key, sizeof(key));
+    printCharStr(ptext, sizeof(ptext));
+
     expandkey128(key,RoundKey);
     aes_encrypt_asm(ptext,RoundKey,Nr);
-    for(int i = 0; i < sizeof(ptext)/sizeof(unsigned char); i++){
-        printf("%02X",ptext[i]);
-    }
-    printf("\n");
+    printCharStr(ptext, sizeof(ptext));
+
     aes_decrypt_asm(ptext,RoundKey,Nr); // should get back the original text
-    for(int i = 0; i < sizeof(ptext)/sizeof(unsigned char); i++){
-        printf("%02X",ptext[i]);
-    }
-    printf("\n");
+    printCharStr(ptext, sizeof(ptext));
+
 }
