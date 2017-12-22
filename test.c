8275c990918778671a60c5d3dda358bef642c8bd
Kyle Roarty <kyleroarty1716@gmail.com>
2017-12-21 20:05:18 -0600

    test: Encrypt/decrypt not multiple of 16B; pad with 0's
    Signed-off-by: Kyle Roarty <kyleroarty1716@gmail.com>

--- test.c
+++ test.c
@@ -1,4 +1,7 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
 
 extern void expandkey128(unsigned char *, unsigned char *);
 extern void aes_encrypt_asm(unsigned char *, unsigned char *, unsigned char *, int);
@@ -14,23 +17,54 @@ void printCharStr(unsigned char *char_str, int len){
     return;
 }
 
+void printPointStr(unsigned char *char_str, int len){
+    for(int i = 0; i < len; i++){
+        printf("%02X", char_str[i]);
+    }
+    printf("\n");
+
+    return;
+}
+
 int main( ){
     unsigned char key[16] = {0x00  ,0x01  ,0x02  ,0x03  ,0x04  ,0x05  ,0x06  ,0x07,
                              0x08  ,0x09  ,0x0a  ,0x0b  ,0x0c  ,0x0d  ,0x0e  ,0x0f};
+
     unsigned char ptext[] = {0x00  ,0x11  ,0x22  ,0x33  ,0x44  ,0x55  ,0x66  ,0x77,
-                             0x88  ,0x99  ,0xaa  ,0xbb  ,0xcc  ,0xdd  ,0xee  ,0xff};
+                             0x88  ,0x99  ,0xaa  ,0xbb  ,0xcc  ,0xdd  ,0xee  ,0xff,
+                             0x00  ,0x11};
     unsigned char RoundKey[240];
-    unsigned char res[16];
+    unsigned char *res, *padded_ptext, *dec_ptext;
     int Nr = 10;
 
+    int ptext_len = sizeof(ptext)/sizeof(unsigned char);
+    int ptext_len_ceil = 16*ceil(ptext_len/16.0);
+
+    res = calloc(ptext_len_ceil, sizeof(unsigned char));
+    dec_ptext = calloc(ptext_len_ceil, sizeof(unsigned char));
+    //Pad ptext with 0's
+    padded_ptext = calloc(ptext_len_ceil, sizeof(unsigned char));
+    memcpy(padded_ptext, ptext, 18);
+
+
     printCharStr(key, sizeof(key));
-    printCharStr(ptext, sizeof(ptext));
+    printPointStr(padded_ptext, ptext_len_ceil);
+
 
     expandkey128(key,RoundKey);
-    aes_encrypt_asm(ptext,RoundKey,res,Nr);
-    printCharStr(res, sizeof(res));
 
-    aes_decrypt_asm(res,RoundKey,ptext,Nr); // should get back the original text
-    printCharStr(ptext, sizeof(ptext));
+    for(int i = 0; i < ptext_len_ceil; i += 16){
+        aes_encrypt_asm(padded_ptext+i, RoundKey, res+i, Nr);
+    }
+    printPointStr(res, ptext_len_ceil);
+
+    for(int i = 0; i < ptext_len_ceil; i += 16){
+        aes_decrypt_asm(res+i, RoundKey, dec_ptext+i, Nr);
+    }
+    printPointStr(dec_ptext, ptext_len_ceil);
+
+    free(padded_ptext);
+    free(dec_ptext);
+    free(res);
 
 }
